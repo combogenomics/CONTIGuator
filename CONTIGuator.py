e71d5b32993159ebef6c39fd5bab3b28d8bd6207
Marco Galardini <galactus8403@yahoo.it>
2012-09-05 11:49:05 +0200

    Version bump

--- CONTIGuator.py
+++ CONTIGuator.py
@@ -8,7 +8,7 @@ __copyright__ = "Copyright 2011-12"
 __credits__ = ["Lee Katz", "Florent Lassalle","Margaret Priest",
                "Luisa Santopolo","Francesca Decorosi","Mitchell Stanton-Cook"]
 __license__ = "GPL"
-__version__ = "2.6.1"
+__version__ = "2.6.2"
 __maintainer__ = "Marco Galardini"
 __email__ = "marco.galardini@unifi.it"
 __status__ = "Production"
